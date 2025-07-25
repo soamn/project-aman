028287f471101bc9c4c81f931343791556848e33
soamn <aman@enumbr.com>
2025-07-25 15:03:54 +0530

    minor fix

--- app/components/AdSense.tsx
+++ app/components/AdSense.tsx
@@ -1,14 +1,12 @@
 // components/AdSense.tsx
-import Script from "next/script";
 
 export default function AdSense() {
   return (
     <>
-      <Script
+      <script
         async
         src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8561760210688511"
         crossOrigin="anonymous"
-        strategy="lazyOnload"
       />
     </>
   );
