146092f5c92a5d29882363e0604f3562922434bf
soamn <aman@enumbr.com>
2026-02-04 22:30:32 +0530

    proxy fix

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { auth } from "./auth";
 
-export async function proxyy(req: NextRequest) {
+export async function proxy(req: NextRequest) {
   const session = await auth();
   if (session) {
     return NextResponse.next();
